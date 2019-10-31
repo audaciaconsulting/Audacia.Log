d17de571a4c99a1838e059986b289689c3aadd12
Adam Drewery <adam.drewery@audacia.co.uk>
2019-10-31 17:26:51 +0000

    Add xml doc comments.

--- Audacia.Log.AspNet4/AiHandleErrorAttribute.cs
+++ Audacia.Log.AspNet4/AiHandleErrorAttribute.cs
@@ -4,9 +4,11 @@ using Microsoft.ApplicationInsights;
 
 namespace Audacia.Log.AspNet4
 {
+	/// <summary>The default error handling filter that application insights uses for ASP.NET 4 projects.</summary>
 	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)]
 	public sealed class AiHandleErrorAttribute : HandleErrorAttribute
 	{
+		/// <inheritdoc />
 		public override void OnException(ExceptionContext filterContext)
 		{
 			if (filterContext?.HttpContext != null && filterContext.Exception != null)
