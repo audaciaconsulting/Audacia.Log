918e329a7be4737ef287777349921e0b8dbf01f0
John Penny <john.penny@audacia.co.uk>
2021-08-02 13:10:15 +0100

    move startup extension from the template project into Audacia.Log

--- Audacia.Log.AspNetCore/ServiceCollectionExtensions.cs
+++ Audacia.Log.AspNetCore/ServiceCollectionExtensions.cs
@@ -1,5 +1,8 @@
 using System;
 using Microsoft.ApplicationInsights.AspNetCore.Extensions;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ApplicationInsights.Extensibility.PerfCounterCollector.QuickPulse;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
@@ -9,6 +12,44 @@ namespace Audacia.Log.AspNetCore
     /// <summary>Extension methods for configuring logging services for an ASP.NET Core application.</summary>
     public static class ServiceCollectionExtensions
     {
+        /// <summary>
+        /// Configures default application insights configuration and logging of request data.
+        /// </summary>
+        /// <exception cref="ArgumentNullException"><paramref name="services"/> is <see langword="null"/>.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="configuration"/> is <see langword="null"/>.</exception>
+        public static IServiceCollection ConfigureApplicationInsights(this IServiceCollection services, IConfiguration configuration)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            if (configuration == null)
+            {
+                throw new ArgumentNullException(nameof(configuration));
+            }
+
+            // Configure Application Insights, configuration will be pulled from the appsettings file
+            // see: https://docs.microsoft.com/en-us/azure/azure-monitor/app/asp-net-core
+            services.AddApplicationInsightsTelemetry();
+
+            // Live metrics stream is enabled by default, configure a secure control channel
+            // see: https://docs.microsoft.com/en-us/azure/azure-monitor/app/live-stream#secure-the-control-channel
+            var quickPulseKey = configuration.GetValue<string>("APPINSIGHTS_QUICKPULSEAUTHAPIKEY") ??
+                                configuration.GetValue<string>("ApplicationInsights:QuickPulseApiKey");
+
+            if (!string.IsNullOrWhiteSpace(quickPulseKey))
+            {
+                services.ConfigureTelemetryModule<QuickPulseTelemetryModule>((module, _) => module.AuthenticationApiKey = quickPulseKey);
+            }
+
+            // Add custom telemetry to requests
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddSingleton<ITelemetryInitializer, RequestDataTelemetryInitialiser>();
+
+            return services;
+        }
+
         /// <summary>Configures logging for an ASP.NET Core application using the specified <see cref="AudaciaLoggerConfiguration"/>.</summary>
         /// <exception cref="ArgumentNullException"><paramref name="services"/> is <see langword="null"/>.</exception>
         /// <exception cref="ArgumentNullException"><paramref name="configuration"/> is <see langword="null"/>.</exception>
